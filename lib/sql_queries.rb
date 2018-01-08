444aa6353387a6e818c00728fd63510ae8831aba
Chris Cardle <cdcardle@gmail.com>
2018-01-08 20:11:16 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -15,7 +15,7 @@ def selects_oldest_bear_and_returns_name_and_age
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "SELECT bears.name, bears.age FROM bears ORDER BY age ASC LIMIT 1;"
+  "SELECT bears.name, bears.age FROM bears GROUP BY age LIMIT 1;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
@@ -23,7 +23,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  ""
+  "SELECT "
 end
 
 def selects_bear_that_killed_Tim
